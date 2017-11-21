1450f70bf24f6db49ef99f618c0a936a0d4ffb90
[Sofoklis] <33422958+politis-sofoklis@users.noreply.github.com>
2017-11-21 15:58:26 +0200

    Initializes Project

---
+++ Excercise 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Excercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string> { "i" ,"t", "t", "t", "k", "t", "t", "t" };
+            List<string> lista1 = Utilities.LongestSubsGen(lista);
+            foreach (string item in lista1)
+            {
+                Console.WriteLine(item);
+            }
+            
+            //int x = 6;
+            //int y = 5;
+            //Utilities.Swap(ref x, ref y);
+            //string z = "stelios";
+            //string k = "kontelas";
+            //Utilities.SwapGen(ref z, ref k);
+            //Console.WriteLine(z);
+            //Console.WriteLine(k);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Excercise 1/Utilities.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Excercise1
+{
+    static class Utilities
+    {
+
+        static public void Swap(ref int x, ref int y)
+        {
+            int temp = y;
+            y = x;
+            x = temp;
+        }
+        static public void SwapGen<T>(ref T x, ref T y)
+        {
+            T temp = y;
+            y = x;
+            x = temp;
+        }
+         static public List<int> LongestSubs(List<int> list)
+        {
+            List<int> longest = new List<int>{list[0]};
+            List<int> templongest = new List<int> {list[0]};
+            for (int i = 0; i < (list.Count-1); i++)
+            {
+                if (list[i + 1] == list[i])
+                {
+                    templongest.Add(list[i+1]);
+                }
+                else
+                {
+                    templongest = new List<int> { list[i+1] };
+                }
+                if (templongest.Count > longest.Count)
+                {
+                    longest = templongest;
+                    
+                }
+            }
+            return longest;
+        }
+
+        static public List<T> LongestSubsGen<T>(List<T> list)
+
+        {
+            List<T> longest = new List<T> { list[0] };
+            List<T> templongest = new List<T> { list[0] };
+            for (int i = 0; i < list.Count -1; i++)
+            {
+                if (list[i + 1].Equals(list[i]))
+                {
+                    templongest.Add(list[i + 1]);
+                }
+                else
+                {
+                    templongest = new List<T> { list[i + 1] };
+                }
+                if (templongest.Count > longest.Count)
+                {
+                    longest = templongest;
+                }
+
+            }
+            return longest;
+        }
+    }
+}
